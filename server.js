48b6d4bc16cb5d97fd4248941881f6dd4d6cf88c
Karan <krnsngh38@gmail.com>
2021-05-11 12:18:02 +0530

    post and mongo connected

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "random_data";
+let db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) return console.log(err);
 
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
+    db = client.db(dbName);
+    const quotesCollection = db.collection("quotes");
+    console.log(`Connected MongoDB: ${url}`);
+    console.log(`Database: ${dbName}`);
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
 
-app.listen(3000, function () {
-  console.log("listening on port 3000");
-});
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.listen(3000, function () {
+      console.log("listening on port 3000");
+    });
+  }
+);
